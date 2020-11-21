96a5eed89201ac7be5961b20b4d97e73e812dee2
pinonpierre <pinon.pierre@gmail.com>
2020-11-21 23:23:52 +0100

    - Add name to Job allowing to trace the sequence during executino

--- server/src/main/java/com/xesnet/sshtaskmanager/model/Job.java
+++ server/src/main/java/com/xesnet/sshtaskmanager/model/Job.java
@@ -7,8 +7,18 @@ import java.util.List;
  * @author Pierre PINON
  */
 public class Job {
+
+    private String name;
     private String run;
     private List<Condition> conditions;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public String getRun() {
         return run;
